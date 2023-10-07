febe45607cafb31b69de96cf56da9b32ecba1380
Sébastien Delpeuch <s.delpeuch@catie.fr>
2023-10-07 17:56:28 +0200

    It introduces a 'bridges.py' file which contains bridge configurations with Gazebo topic, ROS topic, message types and direction. This would facilitate communication between Gazebo simulation and ROS nodes.
    A new file 'bridge.py' is also introduced. This file contains a Bridge class used for defining a bridge between Gazebo and ROS using provided configurations. It also includes a BridgeDirection Enum for specifying the direction of the bridge.
    
    These implementations will ensure seamless integration and communication between the simulation environment and the physical MiniPock bot, allowing for efficient testing and development of functionalities.

---
+++ minipock_gz/bridge.py
@@ -0,0 +1,57 @@
+"""
+Class for a bridge between Gazebo and ROS.
+"""
+
+from dataclasses import dataclass
+from enum import Enum
+
+
+class BridgeDirection(Enum):
+    """
+    Enum for the direction of a bridge.
+    """
+    BIDIRECTIONAL = 0
+    GZ_TO_ROS = 1
+    ROS_TO_GZ = 2
+
+
+DIRECTION_SYMS = {
+    BridgeDirection.BIDIRECTIONAL: '@',
+    BridgeDirection.GZ_TO_ROS: '[',
+    BridgeDirection.ROS_TO_GZ: ']',
+}
+
+
+@dataclass
+class Bridge:
+    """
+    Class for a bridge between Gazebo and ROS.
+
+    :param gz_topic: Gazebo topic name
+    :param ros_topic: ROS topic name
+    :param gz_type: Gazebo message type
+    :param ros_type: ROS message type
+    :param direction: BridgeDirection enum
+    """
+    gz_topic: str
+    ros_topic: str
+    gz_type: str
+    ros_type: str
+    direction: BridgeDirection
+
+    def argument(self):
+        """
+        Return the argument string for the bridge.
+
+        :return: argument string
+        """
+        out = f'{self.gz_topic}@{self.ros_type}{DIRECTION_SYMS[self.direction]}{self.gz_type}'
+        return out
+
+    def remapping(self):
+        """
+        Return the remapping tuple for the bridge.
+
+        :return: remapping tuple
+        """
+        return (self.gz_topic, self.ros_topic)
---
+++ minipock_gz/bridges.py
@@ -0,0 +1,45 @@
+from bluenav_gz import bridge
+
+
+def clock():
+    return bridge.Bridge(
+        gz_topic='/clock',
+        ros_topic='/clock',
+        gz_type='gz.msgs.Clock',
+        ros_type='rosgraph_msgs/msg/Clock',
+        direction=bridge.BridgeDirection.GZ_TO_ROS)
+
+
+def pose(model_name):
+    return bridge.Bridge(
+        gz_topic=f'/model/{model_name}/pose',
+        ros_topic=f'{model_name}/pose',
+        gz_type='gz.msgs.Pose_V',
+        ros_type='tf2_msgs/msg/TFMessage',
+        direction=bridge.BridgeDirection.GZ_TO_ROS)
+
+
+def joint_states(world_name, model_name):
+    return bridge.Bridge(
+        gz_topic=f'/world/{world_name}/model/{model_name}/joint_state',
+        ros_topic=f'{model_name}/joint_states',
+        gz_type='gz.msgs.Model',
+        ros_type='sensor_msgs/msg/JointState',
+        direction=bridge.BridgeDirection.GZ_TO_ROS)
+
+
+def odometry(model_name):
+    return bridge.Bridge(
+        gz_topic=f'/model/{model_name}/odometry',
+        ros_topic=f'{model_name}/odometry',
+        gz_type='gz.msgs.Odometry',
+        ros_type='nav_msgs/msg/Odometry',
+        direction=bridge.BridgeDirection.GZ_TO_ROS)
+
+def cmd_vel(model_name):
+    return bridge.Bridge(
+        gz_topic=f'/cmd_vel',
+        ros_topic=f'{model_name}/cmd_vel',
+        gz_type='gz.msgs.Twist',
+        ros_type='geometry_msgs/msg/Twist',
+        direction=bridge.BridgeDirection.ROS_TO_GZ)
