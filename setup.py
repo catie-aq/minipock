7c42209fc5c5281aac43a2550ff55d6f562c0546
Sébastien Delpeuch <s.delpeuch@catie.fr>
2023-10-13 18:15:28 +0200

    Add setup.py and setup.cfg for the minipock_gz package
    Implemented a setup.py and setup.cfg file to configure the installation of the minipock_gz package.

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from glob import glob
+
+from setuptools import setup
+
+package_name = "minipock_gz"
+
+setup(
+    name=package_name,
+    version='1.0.0',
+    description='MiniPock Worlds for Gazebo Sim',
+    author='Sébastien Delpeuch',
+    author_email='s.delpeuch@catie.fr',
+    license='Apache 2.0',
+    install_requires=[
+        'python>=3.10',
+    ],
+    packages=[package_name],
+    entry_points={
+    },
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        (os.path.join('share', package_name), ['package.xml']),
+        (os.path.join('share', package_name, 'worlds'), glob(os.path.join('worlds', '*.*'))),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.*'))),
+
+    ]
+)
