43ceae6bc464bc9400559523b9c25c5ad6e0e89f
Sébastien Delpeuch <s.delpeuch@catie.fr>
2023-12-08 10:17:22 +0100

    Add launch library and spawn functionality
    A new 'launch' directory is now included in the project, allowing handling of launch configurations through setup.py. Additionally, this commit introduces a spawn functionality, allowing dynamically spawning robot simulations based on specific configurations.

--- launch/spawn.launch.py
+++ launch/spawn.launch.py
@@ -17,9 +17,6 @@ from launch.actions import GroupAction
 from launch.actions import OpaqueFunction
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-from launch_ros.actions import PushRosNamespace
-
-import minipock_description.model
 
 
 def parse_config(context, *args, **kwargs):
@@ -31,43 +28,32 @@ def parse_config(context, *args, **kwargs):
     :param kwargs: variable number of additional keyword arguments
     :return: a list of launch processes
     """
-    x_pos = LaunchConfiguration('x').perform(context)
-    y_pos = LaunchConfiguration('y').perform(context)
-    z_pos = LaunchConfiguration('z').perform(context)
-    r_rot = LaunchConfiguration('R').perform(context)
-    p_rot = LaunchConfiguration('P').perform(context)
-    y_rot = LaunchConfiguration('Y').perform(context)
     robot_name = LaunchConfiguration('robot_name').perform(context)
-    position = [x_pos, y_pos, z_pos, r_rot, p_rot, y_rot]
     launch_processes = []
-    launch_processes.extend(spawn(position, robot_name))
+    launch_processes.extend(spawn(robot_name))
     return launch_processes
 
 
-def spawn(position, robot_name):
+def spawn(robot_name):
     """
-    This method is used to spawn a robot in a simulation environment. It takes two parameters:
-    `position` and `robot_name`.
+    This method is used to spawn a robot in a simulation environment.
+    It reads the URDF file of the robot model and generates launch processes to start the
+    robot_state_publisher node.
 
-    :param position: The position where the robot should be spawned.
     :param robot_name: The name of the robot.
-
-    :return: A list of launch processes that need to be executed to spawn the robot.
+    :return: A list of launch processes.
     """
-    launch_processes = [Node(package='ros_gz_sim', executable='create', output='screen',
-                             arguments=minipock_description.model.spawn_args(position))]
     # robot_state_publisher
-    model_dir = os.path.join(get_package_share_directory('minipock_description'), 'models/tmp')
+    model_dir = os.path.join(get_package_share_directory(f'{robot_name}_description'), 'models/tmp')
     urdf_file = os.path.join(model_dir, 'model.urdf')
     with open(urdf_file) as input_file:
         robot_desc = input_file.read()
-    params = {'use_sim_time': True, 'robot_description': robot_desc,
-              'namespace': robot_name}
+    params = {'use_sim_time': False, 'robot_description': robot_desc}
     nodes = [
         Node(package='robot_state_publisher', executable='robot_state_publisher', output='both',
-             parameters=[params], remappings=[('/joint_states', f'/{robot_name}/joint_states')])]
-    group_action = GroupAction([PushRosNamespace(robot_name), *nodes])
-    launch_processes.append(group_action)
+             parameters=[params])]
+    group_action = GroupAction(nodes)
+    launch_processes = [group_action]
     return launch_processes
 
 
@@ -77,25 +63,7 @@ def generate_launch_description():
 
     :return: A LaunchDescription object.
     """
-    return LaunchDescription([DeclareLaunchArgument('x',
-                                                    default_value='0',
-                                                    description='X position to spawn'),
-                              DeclareLaunchArgument('y',
-                                                    default_value='0',
-                                                    description='y position to spawn'),
-                              DeclareLaunchArgument('z',
-                                                    default_value='1.0',
-                                                    description='z position to spawn'),
-                              DeclareLaunchArgument('R',
-                                                    default_value='0',
-                                                    description='R rotation to spawn'),
-                              DeclareLaunchArgument('P',
-                                                    default_value='0',
-                                                    description='P rotation to spawn'),
-                              DeclareLaunchArgument('Y',
-                                                    default_value='0',
-                                                    description='Y rotation to spawn'),
-                              DeclareLaunchArgument('robot_name',
+    return LaunchDescription([DeclareLaunchArgument('robot_name',
                                                     default_value='minipock',
                                                     description='Robot name'),
                               OpaqueFunction(function=parse_config)])
