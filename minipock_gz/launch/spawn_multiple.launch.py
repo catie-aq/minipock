a45ca24cf730e8b8c1a82a25764296080bb3fa90
Charlène <c.vernant@catie.fr>
2024-07-10 15:26:24 +0000

    refactor: Update use_sim_time default value to False in spawn_multiple.launch.py

--- minipock_gz/launch/spawn_multiple.launch.py
+++ minipock_gz/launch/spawn_multiple.launch.py
@@ -249,7 +249,7 @@ def generate_launch_description():
             ),
             DeclareLaunchArgument(
                 "use_sim_time",
-                default_value="True",
+                default_value="False",
                 description="Use simulation time",
             ),
             OpaqueFunction(function=parse_config),
