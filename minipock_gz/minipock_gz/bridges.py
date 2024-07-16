6d73ec3629e4a1170f5dfe29ad81d9d6d31726dd
Charlène <c.vernant@catie.fr>
2024-07-16 09:14:25 +0000

    feat: Update bridge topics and topics' names to include model_name parameter

--- minipock_gz/minipock_gz/bridges.py
+++ minipock_gz/minipock_gz/bridges.py
@@ -11,10 +11,10 @@ def clock():
     )
 
 
-def tf():
+def tf(model_name):
     return bridge.Bridge(
-        gz_topic="/model/minipock/tf",
-        ros_topic="/tf",
+        gz_topic=f"/model{model_name}/tf",
+        ros_topic=f"/tf",
         gz_type="gz.msgs.Pose_V",
         ros_type="tf2_msgs/msg/TFMessage",
         direction=bridge.BridgeDirection.GZ_TO_ROS,
@@ -23,7 +23,7 @@ def tf():
 
 def pose(model_name):
     return bridge.Bridge(
-        gz_topic=f"/model/{model_name}/pose",
+        gz_topic=f"/model{model_name}/pose",
         ros_topic=f"{model_name}/pose",
         gz_type="gz.msgs.Pose_V",
         ros_type="tf2_msgs/msg/TFMessage",
@@ -33,8 +33,8 @@ def pose(model_name):
 
 def joint_states(world_name, model_name):
     return bridge.Bridge(
-        gz_topic=f"/world/{world_name}/model/{model_name}/joint_state",
-        ros_topic=f"/joint_states",
+        gz_topic=f"/world{world_name}/model{model_name}/joint_state",
+        ros_topic=f"{model_name}/joint_states",
         gz_type="gz.msgs.Model",
         ros_type="sensor_msgs/msg/JointState",
         direction=bridge.BridgeDirection.GZ_TO_ROS,
@@ -43,28 +43,28 @@ def joint_states(world_name, model_name):
 
 def odometry(model_name):
     return bridge.Bridge(
-        gz_topic=f"/model/{model_name}/odometry",
-        ros_topic=f"/odom",
+        gz_topic=f"{model_name}/odom",
+        ros_topic=f"{model_name}/odom",
         gz_type="gz.msgs.Odometry",
         ros_type="nav_msgs/msg/Odometry",
         direction=bridge.BridgeDirection.GZ_TO_ROS,
     )
 
 
-def cmd_vel():
+def cmd_vel(model_name):
     return bridge.Bridge(
-        gz_topic=f"/cmd_vel",
-        ros_topic=f"/cmd_vel",
+        gz_topic=f"{model_name}/cmd_vel",
+        ros_topic=f"{model_name}/cmd_vel",
         gz_type="gz.msgs.Twist",
         ros_type="geometry_msgs/msg/Twist",
         direction=bridge.BridgeDirection.ROS_TO_GZ,
     )
 
 
-def scan_lidar():
+def scan_lidar(model_name):
     return bridge.Bridge(
-        gz_topic=f"/scan_raw",
-        ros_topic=f"/scan_raw",
+        gz_topic=f"{model_name}/scan_raw",
+        ros_topic=f"{model_name}/scan_raw",
         gz_type="gz.msgs.LaserScan",
         ros_type="sensor_msgs/msg/LaserScan",
         direction=bridge.BridgeDirection.GZ_TO_ROS,
