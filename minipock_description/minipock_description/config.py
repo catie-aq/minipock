e7e7c3bada28ccf91e967660f001deb87f966eed
Sébastien Delpeuch <s.delpeuch@catie.fr>
2024-07-24 08:36:45 +0000

    feat: Add JSON schema validation for config file

--- minipock_description/minipock_description/config.py
+++ minipock_description/minipock_description/config.py
@@ -1,6 +1,44 @@
 from ament_index_python.packages import get_package_share_directory
 import yaml
 import os
+import jsonschema
+
+
+def validate_config(config_to_check):
+    schema = {
+        "type": "object",
+        "properties": {
+            "namespace": {"type": "string"},
+            "bringup": {"type": "boolean"},
+            "use_sim_time": {"type": "boolean"},
+            "fleet": {
+                "type": "object",
+                "patternProperties": {
+                    "^[a-z0-9]+$": {
+                        "type": "object",
+                        "properties": {
+                            "mode": {
+                                "type": "string",
+                                "enum": ["differential", "holonomic"],
+                            },
+                            "position": {
+                                "type": ["null", "array"],
+                            },
+                        },
+                        "required": ["mode", "position"],
+                    }
+                },
+            },
+        },
+        "required": ["namespace", "bringup", "use_sim_time", "fleet"],
+    }
+
+    try:
+        jsonschema.validate(config_to_check, schema)
+    except jsonschema.ValidationError as e:
+        raise e
+    except TypeError as e:
+        raise TypeError("Some fleet keys are not strings") from e
 
 
 def config():
@@ -11,4 +49,5 @@ def config():
     config_dict = {}
     for key, value in yaml_data.items():
         config_dict[key] = value
+    validate_config(config_dict)
     return config_dict
