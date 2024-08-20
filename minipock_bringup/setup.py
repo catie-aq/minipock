61c915c0901b03b148737b34f7891eb1dca680a9
Sébastien Delpeuch <s.delpeuch@catie.fr>
2024-08-20 11:41:50 +0000

    refactor: Rename odometry_transform_publisher to raw_data_transformer

--- minipock_bringup/setup.py
+++ minipock_bringup/setup.py
@@ -23,7 +23,7 @@ setup(
     tests_require=["pytest"],
     entry_points={
         "console_scripts": [
-            "odometry_transform_publisher = minipock_bringup.odometry_transform_publisher:main",
+            "raw_data_transformer = minipock_bringup.raw_data_transformer:main",
         ],
     },
 )
