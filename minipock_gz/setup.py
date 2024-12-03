1479bc619fbbefa59d288fe59731c09e44eb96e8
Charlène <c.vernant@catie.fr>
2024-12-03 11:02:20 +0000

    Update shared folder for empty room ground map

--- minipock_gz/setup.py
+++ minipock_gz/setup.py
@@ -26,5 +26,8 @@ setup(
         (os.path.join("share", package_name), ["package.xml"]),
         (os.path.join("share", package_name, "worlds"), glob(os.path.join("worlds", "*.*"))),
         (os.path.join("share", package_name, "launch"), glob(os.path.join("launch", "*.*"))),
+        (os.path.join("share", package_name, "worlds","models"), glob(os.path.join("worlds","models", "*.*"))),
+        (os.path.join("share", package_name, "worlds","models","empty_room_ground"), glob(os.path.join("worlds","models","empty_room_ground", "*.*"))),
+        (os.path.join("share", package_name, "worlds","models","empty_room_ground","meshes"), glob(os.path.join("worlds","models","empty_room_ground","meshes", "*.*"))),
     ],
 )
