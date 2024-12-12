e2edabecc715dd68857b6d0dadbf63eaf7aa50f2
Sébastien Delpeuch <s.delpeuch@catie.fr>
2024-12-12 16:01:45 +0100

    feat: Add initial implementation of minipock_firmware_updater package

---
+++ minipock_firmware_updater/README.md
@@ -0,0 +1,28 @@
+# minipock_firmware_updater
+
+Node for firmware update in µROS
+
+Description and purpose
+
+## API
+
+Definition and description of the public API
+
+## Installation
+
+How to build and install (should reference external tools / workflows)
+
+## Usage
+
+How to use the package (should reference external tools / workflows)
+
+## How to develop
+
+How to develop the package (should reference external tools / workflows)
+
+## License
+
+This project is licensed under Apache 2.0, a permissive open source license that
+allows you to freely use, modify, distribute, and sell your own
+products that include this software. The full text of the license can be
+obtained from the [Apache website](https://www.apache.org/licenses/LICENSE-2.0).
---
+++ minipock_firmware_updater/minipock_firmware_updater/firmware_updater.py
@@ -0,0 +1,223 @@
+from enum import Enum
+from typing import Union
+
+import jsonschema
+import rclpy
+import requests
+from jsonschema.exceptions import ValidationError
+from rclpy.node import Node
+
+from minipock_custom_msgs.srv import (
+    GetChunk,
+    GetChunk_Response,
+    TrigUpdate,
+    TrigUpdate_Response,
+)
+
+STORED_VERSION_SCHEMA = {
+    "type": "object",
+    "patternProperties": {
+        "^v[0-9]+\\.[0-9]+\\.[0-9]+$": {
+            "type": "object",
+            "properties": {
+                "size": {"type": "integer"},
+                "content": {"type": "string"},
+            },
+            "required": ["size", "content"],
+        }
+    },
+    "additionalProperties": False,
+}
+
+
+class ErrorCode(Enum):
+    """
+    ErrorCode is an enumeration that represents various error codes that can be encountered
+    in the firmware updater process.
+    """
+
+    SUCCESS = 0
+    PARAMETER_NOT_DECLARED = 1
+    INVALID_METHOD = 2
+    GITHUB_FETCH_ERROR = 3
+    NO_BIN_FILE = 4
+    BIN_DOWNLOAD_ERROR = 5
+    SCHEMA_VALIDATION_FAILED = 6
+
+
+class FirmwareUpdater(Node):
+    """
+    A ROS2 node that handles firmware updates for the minipock device.
+    """
+
+    def __init__(self):
+        super().__init__("firmware_updater")
+        self.declare_parameter("namespace", "minipock")
+        self.declare_parameter("method", "latest")
+        namespace = self.get_parameter("namespace").get_parameter_value().string_value
+        self.stored_version = {}
+
+        self.github_tags: list = self.__get_github_tags()
+        self.allowed_methods: list = ["latest"] + self.github_tags
+        self.get_logger().info(f"Allowed methods: {self.allowed_methods}")
+
+        self.firmware_update_service = self.create_service(
+            TrigUpdate, f"{namespace}/firmware_update", self.firmware_update_callback
+        )
+
+        self.firmware_chunk_service = self.create_service(
+            GetChunk, f"{namespace}/firmware_update/chunk", self.firmware_chunk_callback
+        )
+
+    def __get_github_tags(self) -> list:
+        url = "https://api.github.com/repos/catie-aq/minipock_zephyr-demo/releases"
+        response = requests.get(url, timeout=10)
+        if response.status_code == 200:
+            releases = response.json()
+            tags = [release["tag_name"] for release in releases]
+            return tags
+        else:
+            self.get_logger().error(
+                f"Failed to fetch tags from GitHub: {response.status_code}"
+            )
+            return []
+
+    def __return_no_update(self, error_code: ErrorCode) -> TrigUpdate_Response:
+        response = TrigUpdate_Response()
+        response.success = error_code.value
+        response.new_version_available = False
+        response.new_version = ""
+        response.size = 0
+        return response
+
+    def __get_method(self) -> Union[str, TrigUpdate_Response]:
+        if self.has_parameter("method"):
+            method = self.get_parameter("method").get_parameter_value().string_value
+        else:
+            self.get_logger().error("Parameter 'method' is not declared.")
+            return self.__return_no_update(ErrorCode.PARAMETER_NOT_DECLARED)
+        if method not in self.allowed_methods:
+            return self.__return_no_update(ErrorCode.INVALID_METHOD)
+        return method
+
+    def __download_and_store_firmware(self, version) -> ErrorCode:
+        url = f"https://api.github.com/repos/catie-aq/minipock_zephyr-demo/releases/tags/{version}"
+        download_response = requests.get(url, timeout=10)
+        if download_response.status_code != 200:
+            self.get_logger().error(
+                f"Failed to fetch release details from GitHub: {download_response.status_code}"
+            )
+            return ErrorCode.GITHUB_FETCH_ERROR
+        release = download_response.json()
+        bin_file = next(
+            (
+                asset
+                for asset in release.get("assets", [])
+                if asset["name"].endswith(".bin")
+            ),
+            None,
+        )
+        if not bin_file:
+            self.get_logger().error("No .bin file found in the release assets.")
+            return ErrorCode.NO_BIN_FILE
+        bin_url = bin_file["browser_download_url"]
+        bin_response = requests.get(bin_url, timeout=10, stream=True)
+        if bin_response.status_code != 200:
+            self.get_logger().error(
+                f"Failed to download .bin file: {bin_response.status_code}"
+            )
+            return ErrorCode.BIN_DOWNLOAD_ERROR
+        content = bytearray()
+        for chunk in bin_response.iter_content(chunk_size=8192):
+            if chunk:
+                content.extend(chunk)
+        json_version = {
+            "size": len(content),
+            "content": content.hex(),
+        }
+        self.stored_version[version] = json_version
+        try:
+            jsonschema.validate(
+                instance=self.stored_version, schema=STORED_VERSION_SCHEMA
+            )
+        except ValidationError as e:
+            self.get_logger().error(
+                f"Stored version schema validation failed: {e.message}"
+            )
+            return ErrorCode.SCHEMA_VALIDATION_FAILED
+        return ErrorCode.SUCCESS
+
+    def firmware_update_callback(self, request, response) -> TrigUpdate_Response:
+        """
+        Callback function to handle firmware update requests.
+
+        Args:
+            request: The request object containing the actual firmware version.
+            response: The response object to be populated with the update information.
+
+        Returns:
+            TrigUpdate_Response: The response object indicating whether a new firmware version
+                                is available, along with the new version details if applicable.
+
+        The function performs the following steps:
+        1. Retrieves the update method.
+        2. Determines the target firmware version based on the update method.
+        3. Validates the actual and target firmware versions.
+        4. Compares the actual firmware version with the target version.
+        5. Downloads and stores the new firmware if an update is available.
+        6. Populates the response object with the update information.
+        """
+        method = self.__get_method()
+        if isinstance(method, TrigUpdate_Response):
+            return method
+
+        if method == "latest":
+            version = self.github_tags[0] if self.github_tags else None
+        else:
+            version = method
+
+        if not version:
+            return self.__return_no_update(ErrorCode.SUCCESS)
+
+        parsed_actual_version = request.actual_version.replace(".", "")[1:]
+        parsed_version = version.replace(".", "")[1:]
+
+        if (
+            len(parsed_actual_version) != 3
+            or len(parsed_version) != 3
+            or not parsed_actual_version.isdigit()
+            or not parsed_version.isdigit()
+            or parsed_actual_version > self.github_tags[0].replace(".", "")[1:]
+        ):
+            return self.__return_no_update(ErrorCode.INVALID_METHOD)
+
+        if parsed_actual_version >= parsed_version:
+            return self.__return_no_update(ErrorCode.SUCCESS)
+
+        error_code: ErrorCode = self.__download_and_store_firmware(version)
+        if error_code != ErrorCode.SUCCESS:
+            return self.__return_no_update(error_code)
+        response.new_version_available = True
+        response.new_version = version
+        response.size = self.stored_version[version]["size"]
+
+        return response
+
+    def firmware_chunk_callback(self, request, response):
+        response = GetChunk_Response()
+        response.chunk_id = request.chunk_id
+        response.chunk_byte = "todo"
+        response.chunk_checksum = 1  # TODO: https://pypi.org/project/crc/
+        return response
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = FirmwareUpdater()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
